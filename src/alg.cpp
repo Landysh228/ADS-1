f9dd7c069ad6241578aaccc0d0b83af725bb2543
Landysh <walfa45milana@mail.ru>
2026-02-28 00:54:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,23 +4,89 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) return false;
+    
+    for (uint64_t i = 2; i * i <= value; ++i) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) return 0;
+    uint64_t count = 0;
+    uint64_t number = 1;
+    while (count < n) {
+        number++;
+        bool isPrime = true;
+        if (number < 2) {
+            isPrime = false;
+        } else {
+            for (uint64_t i = 2; i * i <= number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t candidate = value + 1;
+    while (true) {
+        bool isPrime = true;
+        if (candidate < 2) {
+            isPrime = false;
+        } else if (candidate == 2 || candidate == 3) {
+            isPrime = true;
+        } else if (candidate % 2 == 0 || candidate % 3 == 0) {
+            isPrime = false;
+        } else {
+            for (uint64_t i = 5; i * i <= candidate; i += 6) {
+                if (candidate % i == 0 || candidate % (i + 2) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            return candidate;
+        }
+        candidate++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  if (hbound <= 2) return 0;
+    uint64_t sum = 0;
+    for (uint64_t num = 2; num < hbound; num++) {
+        bool isPrime = true;
+        if (num < 2) {
+            isPrime = false;
+        } else if (num == 2) {
+            isPrime = true;
+        } else if (num % 2 == 0) {
+            isPrime = false;
+        } else {
+            for (uint64_t i = 3; i * i <= num; i += 2) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            sum += num;
+        }
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
