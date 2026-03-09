0f13c365dcfc7dcdc4e475f69b9994a5cd4c0f6c
Landysh <walfa45milana@mail.ru>
2026-03-09 16:02:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,119 +4,62 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
-    for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value < 2) {
+        return false;
+    }
+    if (value == 2) {
+        return true;
+    }
+    if (value % 2 == 0) {
+        return false;
+    }
+    for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) {
             return false;
         }
     }
-  return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0) return 0;
-    uint64_t count = 0;
-    uint64_t number = 1;
-    while (count < n) {
-        number++;
-        bool isPrime = true;
-        if (number < 2) {
-            isPrime = false;
-        } else {
-            for (uint64_t i = 2; i * i <= number; i++) {
-                if (number % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isPrime) {
-            count++;
+    if (n == 0) {
+        return 0;
+    }
+    uint64_t k = 0;
+    uint64_t ch = 0;
+    while (k < n) {
+        ch++;
+        if (checkPrime(ch)) {
+            k++;
         }
     }
-    return number;
+    return ch;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t candidate = value + 1;
-    while (true) {
-        bool isPrime = true;
-        if (candidate < 2) {
-            isPrime = false;
-        } else if (candidate == 2 || candidate == 3) {
-            isPrime = true;
-        } else if (candidate % 2 == 0 || candidate % 3 == 0) {
-            isPrime = false;
-        } else {
-            for (uint64_t i = 5; i * i <= candidate; i += 6) {
-                if (candidate % i == 0 || candidate % (i + 2) == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isPrime) {
-            return candidate;
-        }
-        candidate++;
+    uint64_t cled = value + 1;
+    while (!checkPrime(cled)) {
+        cled++;
     }
+    return cled;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  if (hbound <= 2) return 0;
-    uint64_t sum = 0;
-    for (uint64_t num = 2; num < hbound; num++) {
-        bool isPrime = true;
-        if (num < 2) {
-            isPrime = false;
-        } else if (num == 2) {
-            isPrime = true;
-        } else if (num % 2 == 0) {
-            isPrime = false;
-        } else {
-            for (uint64_t i = 3; i * i <= num; i += 2) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isPrime) {
-            sum += num;
+    uint64_t k = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            k += i;
         }
     }
-    return sum;
+    return k;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  if (hbound <= lbound || hbound < 3) return 0;
-    
-    uint64_t count = 0;
-  
-    for (uint64_t i = lbound; i + 2 < hbound; i++) {
-        bool isFirstPrime = true;
-        if (i < 2) {
-            isFirstPrime = false;
-        } else {
-            for (uint64_t j = 2; j * j <= i; j++) {
-                if (i % j == 0) {
-                    isFirstPrime = false;
-                    break;
-                }
-            }
-        }
-        if (!isFirstPrime) continue;
-        uint64_t second = i + 2;
-        bool isSecondPrime = true;
-        for (uint64_t j = 2; j * j <= second; j++) {
-            if (second % j == 0) {
-                isSecondPrime = false;
-                break;
-            }
-        }
-        if (isSecondPrime) {
-            count++;
-        }
+  uint64_t k = 0;
+  for (uint64_t i = lbound; i < hbound- 2; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      k++;
     }
-    return count;
+  }
+  return k;
 }
